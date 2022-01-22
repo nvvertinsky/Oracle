527a15e5bbfef854c677f045c1f6ae4125d2952c
nvvertinsky <nvvertinsky@gmail.com>
2022-01-22 15:07:27 +0300

    t

--- DeleteDuplicates/DeleteDuplicates.sql
+++ DeleteDuplicates/DeleteDuplicates.sql
@@ -1,14 +1,44 @@
-create table lol (lol_id number(12), lol_name varchar2(240), surname varchar2(240));
+create table persn (persn_id number(12), first_name varchar2(240), last_name varchar2(240));
 
-insert into lol values (1, 'Niko', 'Surname');
-insert into lol values (2, 'Niko', 'Surname');
-insert into lol values (3, 'Jane', 'Surname');
-insert into lol values (4, 'Jane', 'Surname');
-insert into lol values (5, 'Ivan', 'Surname');
+insert into persn (persn_id, first_name, last_name) values (1, 'Nikolay', 'Vertinsky');
+insert into persn (persn_id, first_name, last_name) values (2, 'Nikolay', 'Vertinsky');
+insert into persn (persn_id, first_name, last_name) values (3, 'Vasya', 'Vertinsky');
+insert into persn (persn_id, first_name, last_name) values (4, 'Vladimir', 'Makarov');
+insert into persn (persn_id, first_name, last_name) values (5, 'Max', 'Slepchenkov');
 
-select * from lol
+-- 01. Сколько всего дублей first_name и last_name
+select first_name,
+	     last_name,
+	     count(*) as cnt
+  from persn
+ group by first_name,
+          last_name
+having count(*) > 1
 
-select max(lol_id), lol_name
-from lol
-group by lol_name
-having count(lol_id) > 1
+-- 02. Выбрать все дублирующиеся last_name и first_name
+select *
+  from persn
+ where last_name in (select last_name from persn group by last_name having count(*) > 1)
+   and first_name in (select first_name from persn group by first_name having count(*) > 1)
+   
+
+-- 03. Выбрать все дубликаты first_name и last_name кроме первого 
+select persn.*
+  from persn
+  left outer join (select min(persn_id) as persn_id,
+                          first_name
+                          last_name
+                     from persn
+                    group by first_name, last_name) tmp on persn.persn_id = tmp.persn_id
+ where tmp.persn_id is null
+
+-- 04. Удалить дубликаты, кроме первого
+delete from persn
+ where persn.persn_id in (select persn.persn_id
+                            from persn
+                            left outer join (select min(persn_id) as persn_id,
+                                                    first_name
+                                                    last_name
+                                               from persn
+                                              group by first_name, last_name) tmp on persn.persn_id = tmp.persn_id
+                           where tmp.persn_id is null)
