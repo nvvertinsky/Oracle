68dcd32800655742daf9e3564b38d4f64d751c4e
nvvertinsky <nvvertinsky@gmail.com>
2022-12-06 19:41:02 +0300

    null в индексах

---
+++ Oracle/Оптимизация SQL/Индексы/Индексы и null.sql
@@ -0,0 +1,42 @@
+/*
+Правило:
+  - Индексы со структурой B-дерева не хранят записи содержащие NULL
+  
+Исключения:
+  - Кластерный индекс
+  - Битовые
+
+Преймущества:
+
+*/
+
+
+create table t (x int, y int);
+create unique index t_idx on t(x,y);
+insert into t values (1, 1);
+insert into t values (1, null);
+insert into t values (null, 1);
+insert into t values (null, null);
+
+analyze index t_idx validate structure;
+
+select name,
+	   lf_rows
+  from index_stats;
+
+-- Запрос покажет, что в индексе хранится всегда 3 строки, вместо 4. 
+-- Строка null null не сохранится в индексе.
+-- А если в строке есть поле не null, то такие строки хранятся в индексе.
+
+
+insert into t values (null, null); -- ошибки не будет. Потому что null значения не равны другим null значениям.
+-- Но при агрегировании null строка будет считаться за одну
+-- Поэтому чтобы уникальный ключ точно был уникальным, нужно хотя одна колонка not null в таблице
+select *
+  from t
+ group by t.x, 
+          t.y;
+		  
+insert into t values (1, null); -- будет ошибка уникального значения 
+insert into t values (null, 1); -- будет ошибка уникального значения 
+
