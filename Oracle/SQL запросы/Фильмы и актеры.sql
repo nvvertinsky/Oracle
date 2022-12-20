a8ca2f8da0dc63d0da881cc5ca1a7a34fdfb91ff
nvvertinsky <nvvertinsky@gmail.com>
2022-12-20 18:43:48 +0300

    доп.инфа

---
+++ Oracle/SQL запросы/Фильмы и актеры.sql
@@ -0,0 +1,13 @@
+-- Найти актера 1 и актера 2, которые играли в одном фильме
+
+with fm as (select 'Мистер и миссис Смит' film, '1' actor from dual
+            union all
+            select 'Мистер и миссис Смит' film, '2' actor from dual
+            union all
+            select 'Бесславные ублюдки' film, '1' actor from dual
+            union all
+            select 'Игра на понижение' film, '2' actor from dual )
+select film
+  from fm
+ group by film
+having sum(case actor when '1' then 1 else 0 end) > 0 and sum(case actor when '2' then 1 else 0 end) > 0;
