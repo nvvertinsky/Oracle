41bb49253f8303421c7a047f64794eb5d6049c1d
Nikolay Vertinsky <51744865+nvvertinsky@users.noreply.github.com>
2023-01-09 22:54:53 +0300

    Update lost update.sql

--- Oracle/06. Блокировка и защелкивание данных/lost update.sql
+++ Oracle/06. Блокировка и защелкивание данных/lost update.sql
@@ -17,7 +17,13 @@ insert into lostupdate (val) values (0);
 update lostupdate set val = 1 where val = 0;
 
 -- Сессия 2 
+update lostupdate set val = 2 where val = 0; -- update будет ожидать, когда завершиться транзакция в сессии 1
 
+-- Сессия 1
+commit;
+
+-- Сессия 2
+commit;
 
 
 
