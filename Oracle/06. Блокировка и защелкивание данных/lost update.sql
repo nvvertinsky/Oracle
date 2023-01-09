bc02cc4a67280406c7b2fed460cb7f28bf5ef158
Nikolay Vertinsky <51744865+nvvertinsky@users.noreply.github.com>
2023-01-09 23:30:05 +0300

    Update lost update.sql

--- Oracle/06. Блокировка и защелкивание данных/lost update.sql
+++ Oracle/06. Блокировка и защелкивание данных/lost update.sql
@@ -6,9 +6,15 @@ create table lostupdate (val number(12));
 insert into lostupdate (val) values (0);
 
 -- Сессия 1
+select val from lostupdate where val = 0;
+/* 0 */
+
 update lostupdate set val = val + 1 where val = 0;
 
 -- Сессия 2 
+select val from lostupdate where val = 0;
+/* 0 */
+
 update lostupdate set val = val + 2 where val = 0; -- update будет ожидать, когда завершиться транзакция в сессии 1
 
 -- Сессия 1
@@ -20,3 +26,11 @@ commit;
 /*Если бы транзакции выполнялись последовательно, то значение поля было бы равно 3 
 То есть явное влияние транзакций друг на друга.*/
 
+
+-- Сессия 1
+select val from lostupdate where val = 0;
+
+/* 2 */
+
+-- Сессия 1 не прнимает почему значение стало 2. Ведь она увеличивала значение тольна на 1.
+-- Получается обновление Сессии 1 затерли.
