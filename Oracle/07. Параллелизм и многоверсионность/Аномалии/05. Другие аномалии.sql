49da02a693fa318facf2434ba06d686f9935cbf1
Nikolay Vertinsky <51744865+nvvertinsky@users.noreply.github.com>
2023-01-10 23:07:54 +0300

    Update 05. Другие аномалии.sql

--- Oracle/07. Параллелизм и многоверсионность/Аномалии/05. Другие аномалии.sql
+++ Oracle/07. Параллелизм и многоверсионность/Аномалии/05. Другие аномалии.sql
@@ -1,1 +1,13 @@
+select val from tst;
+/*
+1  null
+2  tstval
+*/
 
+-- Все транзакции уровня repeatable read
+
+-- Транзакция 1 
+update tst set val = tstval where val is null;
+
+-- Транзакция 2
+update tst set val = null where val = 'tstval';
