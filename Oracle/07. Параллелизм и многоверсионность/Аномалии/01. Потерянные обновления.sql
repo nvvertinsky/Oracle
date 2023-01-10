dbb18f96db43b9fe8a98d4bc52fd639b88eecba4
Nikolay Vertinsky <51744865+nvvertinsky@users.noreply.github.com>
2023-01-10 22:06:15 +0300

    Update 01. Потерянные обновления.sql

--- Oracle/07. Параллелизм и многоверсионность/Аномалии/01. Потерянные обновления.sql
+++ Oracle/07. Параллелизм и многоверсионность/Аномалии/01. Потерянные обновления.sql
@@ -1,9 +1,36 @@
+/*Проблема потерянного обновления. 
+Эта проблема проявляется в том, что результаты успешно завершенной операции обновления одной транзакции 
+перекрываются результатами выполнения другой транзакции.*/
+
+create table lostupdate (val number(12));
+insert into lostupdate (val) values (0);
+
 -- Сессия 1
-update tst set val = 1 where val = 0;
+select val from lostupdate where val = 0;
+/* 0 */
+
+update lostupdate set val = val + 1 where val = 0;
+
+-- Сессия 2 
+select val from lostupdate where val = 0;
+/* 0 */
+
+update lostupdate set val = val + 2 where val = 0; -- update будет ожидать, когда завершиться транзакция в сессии 1
+
+-- Сессия 1
+commit;
 
 -- Сессия 2
-select val from tst where val = 0;
-/* 1 */
+commit;
 
--- Если уровень изоляции read uncomitted, то
--- сессия 2 увидит незафиксированные денные сессии 1
+/*Если бы транзакции выполнялись последовательно, то значение поля было бы равно 3 
+То есть явное влияние транзакций друг на друга.*/
+
+
+-- Сессия 1
+select val from lostupdate where val = 0;
+
+/* 2 */
+
+-- Сессия 1 не понимает почему значение стало 2. Ведь она увеличивала значение тольна на 1.
+-- Получается обновление Сессии 1 затерли.
