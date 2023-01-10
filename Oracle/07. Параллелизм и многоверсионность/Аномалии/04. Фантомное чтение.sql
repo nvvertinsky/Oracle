2b143c60857accbf34683d63d7606bec4688bc62
Nikolay Vertinsky <51744865+nvvertinsky@users.noreply.github.com>
2023-01-10 22:55:33 +0300

    Update 04. Фантомное чтение.sql

--- Oracle/07. Параллелизм и многоверсионность/Аномалии/04. Фантомное чтение.sql
+++ Oracle/07. Параллелизм и многоверсионность/Аномалии/04. Фантомное чтение.sql
@@ -7,4 +7,6 @@ insert into tst (id, val) values (3, 'tstval');
 
 -- Транзакция 1
 select val from tst where val = 'tstval';
-/* 0 rows */
+/* 1 rows */
+
+-- Если бы уровень транзакции был repeatable read, то тразакция 1 не нашла ни одной строки
