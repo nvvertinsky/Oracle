a95155c38455e9b8303177239e617408cdd00c09
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-16 20:49:30 +0300

    Update Генерация строк.sql

--- 10. Задачи/Задачи SQL/Генерация строк.sql
+++ 10. Задачи/Задачи SQL/Генерация строк.sql
@@ -1,10 +1,1 @@
- select level as id_row /*,round(dbms_random.value(1,100)) as gen_num*/
-   from dual
-connect by level <= 5;
-
-with r (n) as (select 1 n from dual 
-               union all
-               select n + 1 from r where n < 10
-              )
-select *
-  from r;
+-- Написать запрос который будет генерировать 5 строк
