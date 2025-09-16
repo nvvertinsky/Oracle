85848c9736eb521b6cfaa62561d18506a34ca88b
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-16 20:47:52 +0300

    Update Пересечение дат.sql

--- 10. Задачи/Задачи SQL/Пересечение дат.sql
+++ 10. Задачи/Задачи SQL/Пересечение дат.sql
@@ -10,20 +10,3 @@ insert into client_tariff values
 (1, date'2022-01-01', date'2022-01-10');
 insert into client_tariff values
 (1, date'2022-02-01', date'2022-02-13');
-
-
--- Итоговый запрос: 
--- Достаточно проверить, что b2 > a and a2 < b;
-
--- Условие для пересечения: 
---  Нужно проверять находится ли дата новая дата начала внутри промежутка существующих дат или дата окончания
-
-select count(1)
-  from (select  1 client_id,
-                date'2022-01-10' d_from,
-                date'2022-01-12' d_till
-          from dual) e
-  join client_tariff t
-    on t.client_id = e.client_id
-   and e.d_till > t.date_from
-   and e.d_from < t.date_till;
