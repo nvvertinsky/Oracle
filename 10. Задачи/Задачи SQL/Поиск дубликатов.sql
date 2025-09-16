1ac36249366b54f12cd5b6dad5f9c575524cab6d
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-16 20:46:46 +0300

    Update Поиск дубликатов.sql

--- 10. Задачи/Задачи SQL/Поиск дубликатов.sql
+++ 10. Задачи/Задачи SQL/Поиск дубликатов.sql
@@ -7,38 +7,6 @@ insert into persn (persn_id, first_name, last_name) values (4, 'Vladimir', 'Maka
 insert into persn (persn_id, first_name, last_name) values (5, 'Max', 'Slepchenkov');
 
 -- 01. Сколько всего дублей first_name и last_name
-select first_name,
-	     last_name,
-	     count(*) as cnt
-  from persn
- group by first_name,
-          last_name
-having count(*) > 1
-
 -- 02. Выбрать все дублирующиеся last_name и first_name
-select *
-  from persn
- where last_name in (select last_name from persn group by last_name having count(*) > 1)
-   and first_name in (select first_name from persn group by first_name having count(*) > 1)
-   
-
--- 03. Выбрать все дубликаты first_name и last_name кроме первого 
-select persn.*
-  from persn
-  left outer join (select min(persn_id) as persn_id,
-                          first_name
-                          last_name
-                     from persn
-                    group by first_name, last_name) tmp on persn.persn_id = tmp.persn_id
- where tmp.persn_id is null
-
+-- 03. Выбрать все дубликаты first_name и last_name кроме первого
 -- 04. Удалить дубликаты, кроме первого
-delete from persn
- where persn.persn_id in (select persn.persn_id
-                            from persn
-                            left outer join (select min(persn_id) as persn_id,
-                                                    first_name
-                                                    last_name
-                                               from persn
-                                              group by first_name, last_name) tmp on persn.persn_id = tmp.persn_id
-                           where tmp.persn_id is null)
