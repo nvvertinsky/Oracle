c4538212cd90a8a65e0b6d8de6692b1516565076
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-16 20:36:39 +0300

    Update Flashback.sql

--- 10. Задачи/Яндекс/Flashback.sql
+++ 10. Задачи/Яндекс/Flashback.sql
@@ -1,5 +1,3 @@
 /*Рассказать про Flashback. Какие нужны права. Написать запрос с просмотром данных на 1 час назад.*/
 
 grant flashback any table to nvvertinskiy;
-
-select * from t as of scn timestamp_to_scn(sysdate - (1/24))
