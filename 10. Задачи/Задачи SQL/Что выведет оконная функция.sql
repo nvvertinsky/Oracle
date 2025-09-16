e149c745ef0c19a211f0e2d371a570e27c1e11c4
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-16 20:39:58 +0300

    Update Что выведет оконная функция.sql

--- 10. Задачи/Задачи SQL/Что выведет оконная функция.sql
+++ 10. Задачи/Задачи SQL/Что выведет оконная функция.sql
@@ -3,7 +3,6 @@ with tst as (select 1 lvl from dual
              select level lvl from dual connect by level <= 5)
 
 select lvl
-       ,sum(lvl) over (order by lvl) -- Сначала возьмет первую строку и посчитает по ней сумму. Если есть одинаковые строки, то посчитает сумму по всем этим строкам.
-                                     -- Затем берем следующую строку и прибавляем ее значение к общей сумме.
-       ,sum(lvl) over (partition by lvl order by lvl) -- Тоже самое, только бьем еще на партиции
+       ,sum(lvl) over (order by lvl)
+       ,sum(lvl) over (partition by lvl order by lvl)
   from tst;
