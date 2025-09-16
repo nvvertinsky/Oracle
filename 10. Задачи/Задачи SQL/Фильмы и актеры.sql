09fce718734f2337aaa83865d5abee9526fe9c0b
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-16 20:40:48 +0300

    Update Фильмы и актеры.sql

--- 10. Задачи/Задачи SQL/Фильмы и актеры.sql
+++ 10. Задачи/Задачи SQL/Фильмы и актеры.sql
@@ -7,7 +7,3 @@ with fm as (select 'Мистер и миссис Смит' film, '1' actor from 
             select 'Бесславные ублюдки' film, '1' actor from dual
             union all
             select 'Игра на понижение' film, '2' actor from dual )
-select fm.film
-  from fm
- group by fm.film
-having sum(case fm.actor when '1' then 1 else 0 end) > 0 and sum(case fm.actor when '2' then 1 else 0 end) > 0
