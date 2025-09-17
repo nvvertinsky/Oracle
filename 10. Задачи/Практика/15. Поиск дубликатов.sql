98f7c081ebc7a24f7f229106f7aebabce1f8de7e
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-17 20:29:45 +0300

    Update 15. Поиск дубликатов.sql

--- 10. Задачи/Практика/15. Поиск дубликатов.sql
+++ 10. Задачи/Практика/15. Поиск дубликатов.sql
@@ -1,10 +1,10 @@
 create table persn (persn_id number(12), first_name varchar2(240), last_name varchar2(240));
 
-insert into persn (persn_id, first_name, last_name) values (1, 'Nikolay', 'Vertinsky');
-insert into persn (persn_id, first_name, last_name) values (2, 'Nikolay', 'Vertinsky');
-insert into persn (persn_id, first_name, last_name) values (3, 'Vasya', 'Vertinsky');
-insert into persn (persn_id, first_name, last_name) values (4, 'Vladimir', 'Makarov');
-insert into persn (persn_id, first_name, last_name) values (5, 'Max', 'Slepchenkov');
+insert into persn values (1, 'Nikolay', 'Vertinsky');
+insert into persn values (2, 'Nikolay', 'Vertinsky');
+insert into persn values (3, 'Vasya', 'Vertinsky');
+insert into persn values (4, 'Vladimir', 'Makarov');
+insert into persn values (5, 'Max', 'Slepchenkov');
 
 -- 01. Сколько всего дублей first_name и last_name
 -- 02. Выбрать все дублирующиеся last_name и first_name
