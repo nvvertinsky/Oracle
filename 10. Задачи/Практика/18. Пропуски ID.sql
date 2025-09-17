fea18eeb9bf40c842b8dda7750c5df7bb1928fc5
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-17 20:24:52 +0300

    Update 18. Пропуски ID.sql

--- 10. Задачи/Практика/18. Пропуски ID.sql
+++ 10. Задачи/Практика/18. Пропуски ID.sql
@@ -2,24 +2,5 @@
 Есть таблица t c колонкой id.
 В id хранятся целые положительные числа. Например: 1,2,3,4,5,6,10,11,12
 Необходимо найти все ID которые пропущены
-
 */
 
-with t as (select 1 id from dual
-           union all 
-           select 2 from dual
-           union all 
-           select 3 from dual
-           union all 
-           select 4 from dual
-           union all 
-           select 5 from dual
-           union all 
-           select 6 from dual
-           union all 
-           select 10 from dual
-           union all 
-           select 11 from dual
-           union all 
-           select 12 from dual),
-    t2 as (select level from dual connect by level <= 12)
