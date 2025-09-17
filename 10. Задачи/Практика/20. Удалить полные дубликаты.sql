6e38a68f0ab683e5cce29b73c4dcf6cc6761a961
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-17 19:33:48 +0300

    Update 20. Удалить полные дубликаты.sql

--- 10. Задачи/Практика/20. Удалить полные дубликаты.sql
+++ 10. Задачи/Практика/20. Удалить полные дубликаты.sql
@@ -13,13 +13,18 @@ insert into client_balance values (3, 'nikolay3', to_date('02.01.2024', 'dd.mm.y
 insert into client_balance values (4, 'nikolay4', to_date('03.01.2024', 'dd.mm.yyyy'), 400);
 
 
-select b.cl_id,
-       b.cl_name,
-       b.dt,
-       b.balance
-  from client_balance b
- group by b.cl_id,
-          b.cl_name,
-          b.dt,
-          b.balance
-having count(*) > 1
+select rowid
+  from client_balance bb
+ where (bb.cl_id,
+        bb.cl_name,
+        bb.dt,
+        bb.balance) in (select b.cl_id,
+                               b.cl_name,
+                               b.dt,
+                               b.balance
+                          from client_balance b
+                         group by b.cl_id,
+                                  b.cl_name,
+                                  b.dt,
+                                  b.balance
+                        having count(*) > 1)
