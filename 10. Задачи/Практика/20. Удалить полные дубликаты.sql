8dc838717ed8aeb393ea051609e1241f0ade09ca
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-17 19:30:30 +0300

    Update 20. Удалить полные дубликаты.sql

--- 10. Задачи/Практика/20. Удалить полные дубликаты.sql
+++ 10. Задачи/Практика/20. Удалить полные дубликаты.sql
@@ -1,5 +1,6 @@
-3. В таблице ниже появились полные дубликаты. Нужно дубликаты удалить и оставить только 1 запись от них.
-Таблица: client_balance(cl_id /*ID клиента*/, cl_name /*Имя клиента*/, dt /*Остаток на день*/, balance /*Баланс на эту дачу*/);
+В таблице ниже появились полные дубликаты. Нужно дубликаты удалить и оставить только 1 запись от них.
+
+client_balance(cl_id /*ID клиента*/, cl_name /*Имя клиента*/, dt /*Остаток на день*/, balance /*Баланс на эту дачу*/);
 
 create table client_balance(cl_id number, cl_name varchar2(240), dt date, balance number(12));
 insert into client_balance values (1, 'nikolay', to_date('01.01.2024', 'dd.mm.yyyy'), 100);
@@ -10,3 +11,15 @@ insert into client_balance values (3, 'nikolay3', to_date('02.01.2024', 'dd.mm.y
 insert into client_balance values (3, 'nikolay3', to_date('02.01.2024', 'dd.mm.yyyy'), 300);
 insert into client_balance values (3, 'nikolay3', to_date('02.01.2024', 'dd.mm.yyyy'), 300);
 insert into client_balance values (4, 'nikolay4', to_date('03.01.2024', 'dd.mm.yyyy'), 400);
+
+
+select b.cl_id,
+       b.cl_name,
+       b.dt,
+       b.balance
+  from client_balance b
+ group by b.cl_id,
+          b.cl_name,
+          b.dt,
+          b.balance
+having count(*) > 1
