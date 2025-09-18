e80748cd982caaeb885b52fc36ae1adb0fa7f51b
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-18 19:59:24 +0300

    Update 06. Иерархический запрос.sql

--- 10. Задачи/Практика/06. Иерархический запрос.sql
+++ 10. Задачи/Практика/06. Иерархический запрос.sql
@@ -1,5 +1,5 @@
 /*
-Дана таблица TBL
+Дана таблица T
  ID   PARENT_ID  NAME
  ----------------------
  10   [NULL]     Подр_4
@@ -11,8 +11,15 @@
   Подр_3
 */
 
-drop table tbl;
-create table tbl (id number(12), parent_id number(12), name varchar2(240));
-insert into tbl values (10, null, 'Подр_4');
-insert into tbl values (2, 10, 'Подр_3');
-insert into tbl values (3, 10, 'Подр_2');
+drop table t;
+create table t (id number(12), parent_id number(12), name varchar2(240));
+insert into t values (10, null, 'Подр_4');
+insert into t values (2, 10, 'Подр_3');
+insert into t values (3, 10, 'Подр_2');
+
+
+select lpad(' ', name, )
+  from t
+connect by prior id = parent_id
+  start with parent_id is null
+order sibling by name
