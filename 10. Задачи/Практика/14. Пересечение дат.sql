a12051b783ea877d5f269bb6df4e0dbd8b28d9b2
Николай Вертинский <51744865+nvvertinsky@users.noreply.github.com>
2025-09-18 18:48:08 +0300

    Update 14. Пересечение дат.sql

--- 10. Задачи/Практика/14. Пересечение дат.sql
+++ 10. Задачи/Практика/14. Пересечение дат.sql
@@ -1,12 +1,12 @@
--- Задача: при вставке в таблицу необходимо проверять, что новая строка не пересекается по датам с уже имеющимися в разрезе клиента. 
+-- Задача: при вставке в таблицу необходимо проверять, что новая строка не пересекается по датам с уже имеющимися в разрезе id. 
 -- Необходимо написать запрос проверяющий пересечения.
 
-create table client_tariff(
-  client_id number(38) not null,
+create table t(
+  id number(38) not null,
   date_from date not null,
   date_till date not null);
 
-insert into client_tariff values
-(1, date'2022-01-01', date'2022-01-10');
-insert into client_tariff values
-(1, date'2022-02-01', date'2022-02-13');
+insert into t values
+(1, '01.01.2022', '10.01.2022');
+insert into t values
+(1, '01.02.2022', '13.02.2022');
