66047a73bfcd38165e8e761479ad50c69498648b
nvvertinsky <nvvertinsky@gmail.com>
2025-09-22 20:19:27 +0300

    1

--- 10. Задачи/Практика/05. Дебит-Кредит.sql
+++ 10. Задачи/Практика/05. Дебит-Кредит.sql
@@ -6,33 +6,33 @@ acc(acc_id /*Счет*/)
 Нужно найти разность credit от debit по каждому счету по каждому дню.
 
 Итоговый результат: 
-01.01.2024	0001	2500
-01.01.2024	0002	-300
-01.01.2024	0003	0
-01.01.2024	0004	0
-02.01.2024	0001	0
-02.01.2024	0002	9000
-02.01.2024	0003	0
-02.01.2024	0004	0
-03.01.2024	0001	0
-03.01.2024	0002	0
-03.01.2024	0003	0
-03.01.2024	0004	20
+01.01.2024  0001  2500
+01.01.2024  0002  -300
+01.01.2024  0003  0
+01.01.2024  0004  0
+02.01.2024  0001  0
+02.01.2024  0002  9000
+02.01.2024  0003  0
+02.01.2024  0004  0
+03.01.2024  0001  0
+03.01.2024  0002  0
+03.01.2024  0003  0
+03.01.2024  0004  20
 
 
-with debit as (select 0001 acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 1000 amnt from dual
+with debit as (select '0001' acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 1000 amnt from dual
                union all 
-               select 0001 acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 2000 amnt from dual
+               select '0001' acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 2000 amnt from dual
                union all 
-               select 0002 acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 500 amnt from dual
+               select '0002' acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 500 amnt from dual
                /*По 3-му счету нет поступлений*/
                union all 
-               select 0002 acc_id, to_date('02.01.2024', 'dd.mm.yyyy') dt, 9000 amnt from dual
+               select '0002' acc_id, to_date('02.01.2024', 'dd.mm.yyyy') dt, 9000 amnt from dual
                union all 
-               select 0004 acc_id, to_date('03.01.2024', 'dd.mm.yyyy') dt, 20 amnt from dual),
-    credit as (select 0001 acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 500 amnt from dual
+               select '0004' acc_id, to_date('03.01.2024', 'dd.mm.yyyy') dt, 20 amnt from dual),
+    credit as (select '0001' acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 500 amnt from dual
                union all 
-               select 0002 acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 800 amnt from dual
+               select '0002' acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 800 amnt from dual
                /*По 3-му счету нет расходов*/),
     acc as (select '0001' acc_id from dual
             union all
@@ -41,5 +41,34 @@ with debit as (select 0001 acc_id, to_date('01.01.2024', 'dd.mm.yyyy') dt, 1000 
             select '0003' acc_id from dual
             union all
             select '0004' acc_id from dual)
-			
-			
+
+select v.dt,
+       v.acc_id,
+       coalesce(sum(v2.damnt) - sum(v2.camnt), 0) amnt
+  from (select *
+          from acc,
+               (select to_date('01.01.2024', 'dd.mm.yyyy') - 1 + level dt 
+                  from dual
+               connect by level <= 4) dd) v,
+       (select d.dt,
+               d.acc_id,
+               sum(d.amnt) damnt,
+               0 camnt
+          from debit d
+         group by d.dt,
+                  d.acc_id
+         union all 
+        select c.dt,
+               c.acc_id,
+               0 camnt,
+               sum(c.amnt) camnt
+          from credit c
+         group by c.dt,
+                  c.acc_id) v2
+ where v2.dt(+) = v.dt
+   and v2.acc_id(+) = v.acc_id
+ group by v.dt,
+          v.acc_id
+ order by v.dt,
+          v.acc_id
+      
